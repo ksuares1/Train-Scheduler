1ce897ff2a38f6e40771c4b836b80f50f6aece7a
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-12-10 19:50:20 -0500

    added firebase config to js

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCXwVwccn9piLscffNP3imTKq3WbJ-f3NE",
+    authDomain: "train-scheduler-bede7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-bede7.firebaseio.com",
+    projectId: "train-scheduler-bede7",
+    storageBucket: "train-scheduler-bede7.appspot.com",
+    messagingSenderId: "300780472675",
+    appId: "1:300780472675:web:e1b925e92c3d0341bf2b56"
+  };
+
+  firebase.initializeApp(firebaseConfig);
